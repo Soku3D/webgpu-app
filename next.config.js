37c34e3b963620469511ef991e668fe95f3761b6
Soku3D <sonwon39@naver.com>
2023-11-07 19:32:43 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'webgpu-app'
+const nextConfig = {}
 
-module.exports = {
-  assetPrefix: !debug ? `/${name}/` : '',
-}
+module.exports = nextConfig
